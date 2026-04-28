0de6ea189aef548f4a75b38c3e8c04b30df5cc3c
vikneshr46-git <vikneshwaranr46@gmail.com>
2026-04-28 14:57:41 +0530

    Create server.js

---
+++ server.js
@@ -0,0 +1,15 @@
+import express from "express";
+import path from "path";
+
+const app = express();
+const PORT = process.env.PORT || 8080;
+
+app.use(express.static("dist"));
+
+app.get("*", (req, res) => {
+  res.sendFile(path.resolve("dist/index.html"));
+});
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
